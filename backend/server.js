75eaa5b03cba4c4d40a7d0250e8a52e8e00ace1b
Evande1 <crossjab@gmail.com>
2022-06-23 07:59:10 +0800

    fixes #9

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { databaseSource } = require('./config');
+const cors = require('cors')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//can also use origin to specify domains
+app.use(cors());
+//enable pre-flight across the board
+app.options('*', cors())
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect(databaseSource, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Database Connected Successfully!!");    
+}).catch(err => {
+  console.log('Could not connect to the database', err);
+  process.exit();
+});
+
+app.listen(8000, () => {
+  console.log('listening on 8000');
+});
+
+app.get('/', (req, res) => {
+  
+  res.json({"message": "Hello Crud Node Express"});
+});
+
+const MealRoute = require('./routes/Meal')
+
+app.use('/api/meal', MealRoute)
+
+//app.get(endpoint, callback) handle get request with get method
+//./node_modules/.bin/nodemon server.js -> run this command
