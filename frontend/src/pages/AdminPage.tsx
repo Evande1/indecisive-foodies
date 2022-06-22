cc7a920bb6ab3c75e9fe543737bf24d6eaaeca5b
JunJie <tjunjie2000@gmail.com>
2022-06-22 20:11:53 +0800

    feat: add admin page

---
+++ frontend/src/pages/AdminPage.tsx
@@ -0,0 +1,63 @@
+
+import { Box } from "@mui/system";
+
+import {useEffect, useState} from "react";
+import axios from "axios";
+import config from "../config.json";
+import FoodTable from "../components/FoodTable/FoodTable";
+
+    type mealData = {
+        _id: string,
+        meal: string,
+        category: string
+    }
+    
+    const url = config.server.url;
+    // //const headers
+    const AdminPage = () => {
+    
+        const [mealList, setMealList] = useState<mealData[]>([]);
+        
+        useEffect(() => {
+            getAllMeals();
+        }, []);
+    
+        const getAllMeals = () => {
+            axios.get(`${url}/meal`)
+                .then((response) => {
+                    const allMeals: mealData[] = response.data;
+                    setMealList(allMeals);
+                })
+                .catch(error => console.log(`Error: ${error}`))
+        }
+    return (
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItem: 'center',
+            padding: '25px',
+            height: 'auto',
+            width: '100vw',
+          }}
+        >
+            
+          <Box
+            sx={{
+              width: '90%',
+              background: 'white',
+              borderRadius: '16px',
+              paddingY: 5,
+              paddingX: 7,
+            }}
+            > 
+            <FoodTable meals={mealList}/>
+              </Box>
+          </Box>
+        
+      );
+
+}
+
+export default AdminPage;
