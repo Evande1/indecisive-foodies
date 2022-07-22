0fa30818e34a4397aa13cb7f81c2d12b42cc5ad4
JunJie <tjunjie2000@gmail.com>
2022-07-22 19:41:46 +0800

    fix merge conflict

--- frontend/src/MainRouter.tsx
+++ frontend/src/MainRouter.tsx
@@ -2,12 +2,9 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FormPage from "./pages/FormPage";
 import AdminPage from "./pages/AdminPage";
-<<<<<<< HEAD
 import SchedulerPage from "./pages/SchedulerPage";
-=======
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
->>>>>>> c1103ec5380bcc4b8416d9445681d4ad145f5bda
 
 const MainRouter = () => {
     return (
@@ -15,12 +12,9 @@ const MainRouter = () => {
             <Route path="/form" element={ <FormPage/> }/>
             <Route path="" element={ <HomePage/> }/>
             <Route path="/admin" element={<AdminPage/>}/>
-<<<<<<< HEAD
             <Route path="/scheduler" element={<SchedulerPage/>}/>
-=======
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="/register" element={<RegisterPage/>}/>
->>>>>>> c1103ec5380bcc4b8416d9445681d4ad145f5bda
         </Routes>
     )
 }
